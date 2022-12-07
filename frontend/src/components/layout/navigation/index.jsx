850c96df73b1320ec518a952875e30a296af963f
Christin Schade <christinabbe90gmail.com>
2022-12-07 18:15:20 +0100

    frontend and backend

--- frontend/src/components/layout/navigation/index.jsx
+++ frontend/src/components/layout/navigation/index.jsx
@@ -7,9 +7,10 @@ export default function Navigation() {
       <div>
         <ul className="nav-links">   
               <Link to="/index" >Home </Link>
+              <input type="text" placeholder="Suche Metropole..."/>
               <Link to="/login" >Login </Link>
-              <Link to="/detail" >Detail </Link>
-             <Link to="/signup" >Signup </Link>
+              <Link to="/signup" >Signup </Link>
+            
         </ul>
     </div>
   )
